eb25f000af07abc12b30fdfdb2098c9454313607
amarenyo <amarenyo@gmail.com>
2026-01-02 16:15:58 +0100

    added death messeges to DeathListener

--- src/main/java/ch/amarenyo/moneyPlugin/listeners/DeathListener.java
+++ src/main/java/ch/amarenyo/moneyPlugin/listeners/DeathListener.java
@@ -20,12 +20,15 @@ public class DeathListener implements Listener {
             }
         } else {
             Player killer = player.getKiller();
-            String killeruuid = player.getUniqueId().toString();
+            String killeruuid = killer.getUniqueId().toString();
             if (main.getMoney(uuid) >= 100) {
                 main.getConfig().set("money." + uuid, main.getMoney(uuid) - 100);
                 main.getConfig().set("money." + killeruuid, main.getMoney(killeruuid) + 40);
+                player.sendMessage("You lost 100!");
+                killer.sendMessage("You got 40!");
             } else {
                 main.getConfig().set("money." + uuid, 0);
+                player.sendMessage("You lost everything!");
             }
         }
         main.saveConfig();
