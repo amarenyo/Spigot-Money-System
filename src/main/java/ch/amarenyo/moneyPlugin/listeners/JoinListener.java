c2b4a38a435bb0644f76fdda2d12879845b5e08d
amarenyo <amarenyo@gmail.com>
2026-01-02 17:07:56 +0100

    added @EventHandler to JoinListener

--- src/main/java/ch/amarenyo/moneyPlugin/listeners/JoinListener.java
+++ src/main/java/ch/amarenyo/moneyPlugin/listeners/JoinListener.java
@@ -2,10 +2,13 @@ package ch.amarenyo.moneyPlugin.listeners;
 
 import ch.amarenyo.moneyPlugin.Main;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class JoinListener implements Listener{
+
+    @EventHandler
     public void onJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
 
