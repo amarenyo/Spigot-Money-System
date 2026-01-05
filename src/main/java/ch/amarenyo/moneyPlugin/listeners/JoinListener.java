25f81dd7cacdbf0207f20d308aa22f6868cba589
amarenyo <amarenyo@gmail.com>
2026-01-05 10:19:07 +0100

    Refactor player join event handling

--- src/main/java/ch/amarenyo/moneyPlugin/listeners/JoinListener.java
+++ src/main/java/ch/amarenyo/moneyPlugin/listeners/JoinListener.java
@@ -11,10 +11,10 @@ public class JoinListener implements Listener{
     @EventHandler
     public void onJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
-
-        if(!player.hasPlayedBefore()) {
-            Main main = Main.getPlugin(Main.class);
-            String uuid = player.getUniqueId().toString();
+        String uuid = player.getUniqueId().toString();
+        Main main = Main.getPlugin(Main.class);
+        
+        if(!main.getConfig().contains("money." + uuid)) {
             main.getConfig().set("money." + uuid, 2000);
             player.sendMessage("You got 2000 as a starter bonus!");
             main.saveConfig();
